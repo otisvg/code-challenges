f784ca0aae526000d4ab63977b53e71048f19575
Otis <otisvickersgraver@gmail.com>
2021-01-24 16:44:34 +0000

    returns hashed input

--- maskify/lib/maskify.rb
+++ maskify/lib/maskify.rb
@@ -2,6 +2,10 @@ class Test
 
   def maskify(input)
     return input if input.length < 5
+    first_half_length = input[0.. input.length - 5].length
+    hash = '#' * first_half_length
+    last_four =  input[input.length - 4.. input.length - 1]
+    hash.concat(last_four)
   end
 
 end
--- maskify/spec/maskify_spec.rb
+++ maskify/spec/maskify_spec.rb
@@ -1,19 +1,35 @@
 require 'maskify'
 
 describe Test do 
-  before(:each) do
-    @test = Test.new
-  end
-  
   describe '.maskify' do 
+
+    before(:each) do
+      @test = Test.new
+    end
+
     it 'returns input if its length is less than 5' do
       expect(@test.maskify('Temi')).to eq 'Temi'
     end
+
     it 'returns input if its length is less than 5' do
       expect(@test.maskify('29c6')).to eq '29c6'
     end
+
     it 'returns input if its length is less than 5' do
       expect(@test.maskify('')).to eq ''
     end
+
+    it 'returns hashed input if length is 5 or greater' do
+       expect(@test.maskify('thisisagreatpassword')).to eq '################word'
+    end
+
+    it 'returns hashed input if length is 5 or greater' do
+       expect(@test.maskify('1532590483694224')).to eq '############4224'
+    end
+
+    it 'returns hashed input if length is 5 or greater' do
+       expect(@test.maskify('hunter2')).to eq '###ter2'
+    end
   end
+
 end
