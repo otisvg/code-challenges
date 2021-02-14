8f46f9e358ae40939197e719fac3a52cf37b539c
Otis <otisvickersgraver@gmail.com>
2021-02-14 18:02:36 +0000

    rename count_initials to initials

--- count-chars/lib/name_count.rb
+++ count-chars/lib/name_count.rb
@@ -2,15 +2,15 @@ class NameCount
   
   def initialize(letters:)
     @letters = letters
-    @count_initials = {}
+    @initials = {}
   end
   
   def calculate
     empty_string?
     unique_letters.each do |letter|
-      @count_initials[letter] = @letters.count(letter)
+      @initials[letter] = @letters.count(letter)
     end
-    @count_initials
+    @initials
   end
   
 
