593d3a1ae75cbac9e18efd813d62867f186be542
Otis <otisvickersgraver@gmail.com>
2021-02-20 22:35:20 +0000

    Masks input

--- break-camelcase/__test__/breakCamelCase.test.js
+++
@@ -1,9 +0,0 @@
-const { maskify } = require('../breakCamelCase');
-
-describe("maskify", () => {
-  it("should not change last four character of input", () => {
-    let input = "35542";
-    let output = "#5542"
-    expect(maskify(input)).toBe(output);
-  });
-});
--- break-camelcase/maskify.js
+++ break-camelcase/maskify.js
@@ -3,12 +3,11 @@ function maskify(string) {
   let sliced_string = string.slice(0, string.length - 4)
   let hashed = "#".repeat(sliced_string.length)
   let last_four = string.substr(string.length - 4)
-  let result = hashed + last_four
-  return result
+  return hashed + last_four
 }
 
 function isUnderFive(string) {
-  if (string.length <=4) { return string }
+  if (string.length < 5) { return string }
 }
 
 module.exports = { maskify }
